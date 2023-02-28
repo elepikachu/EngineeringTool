15e84c14b434e82b2e2378be3ce293a14760c2ff
Elepikachu <568740695@qq.com>
2023-02-28 22:57:18 +0800

    selection

--- EngineeringTool/urls.py
+++ EngineeringTool/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_view),
+    path('ikun', views.cxk_view),
+    path('choose', views.choose_view),
 ]
--- EngineeringTool/views.py
+++ EngineeringTool/views.py
@@ -10,3 +10,13 @@ VERSION = 'Engineering Toolbox 1.0.0'
 def main_view(request):
     dic = {'ver': VERSION}
     return render(request, 'main.html', dic)
+
+
+def cxk_view(request):
+    dic = {'ver': VERSION}
+    return render(request, 'kun.html', dic)
+
+
+def choose_view(request):
+    dic = {'ver': VERSION}
+    return render(request, 'choose.html', dic)
